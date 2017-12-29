9dc07f2982806dd8c8d8da68feb50eb3574a74d3
Clément Michel <m1ch3lcl@gmail.com>
2017-12-29 13:27:22 +0100

    update router to fit apache requirements

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
     $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
     if (preg_match("/^\/api/i", $path)) {
       include "./api/api.php";
-    } else {
+    } else if ($path != "/" && file_exists(substr($path, 1))) {
       return false;
+    } else {
+      include "./frontend/index.html";
     }
 ?>
