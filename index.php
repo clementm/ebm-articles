88a12955a71f30d04205c2530a433cb0b1c05287
Clément Michel <m1ch3lcl@gmail.com>
2017-12-29 17:42:19 +0100

    update router configuration

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     if (preg_match("/^\/api/i", $path)) {
       include "./api/api.php";
     } else if ($path != "/" && file_exists(substr($path, 1))) {
+      echo file_get_contents(substr($path, 1));
       return false;
     } else {
       include "./frontend/index.html";
